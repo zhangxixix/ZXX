38db683fc08cfc675f5d5c5aa1d053951eef8fa8
111 <111>
2017-04-07 09:13:48 +0800

    dd

--- src/main/java/cn/sean/zxx/GetAllSubSequences.java
+++ src/main/java/cn/sean/zxx/GetAllSubSequences.java
@@ -1,52 +1,64 @@
 package cn.sean.zxx;
 
-public class GetAllSubSequences {
+public class GetAllSubSequences
+{
 
-	public static void main(String[] args) {
-		System.out.println(subsequences("abc"));
-	}
+    public static void main(String[] args)
+    {
+        System.out.println(subsequences("abc"));
+    }
 
-	/**
-	 * Return all subsequences of word (as defined above) separated by commas,
-	 * with partialSubsequence prepended to each one.
-	 */
-	@SuppressWarnings("unused")
-	private static String subsequencesAfter(String partialSubsequence, String word) {
-		if (word.isEmpty()) {
-			// base case
-			return partialSubsequence;
-		} else {
-			// recursive step
-			return subsequencesAfter(partialSubsequence, word.substring(1)) + ","
-					+ subsequencesAfter(partialSubsequence + word.charAt(0), word.substring(1));
-		}
-	}
+    /**
+     * Return all subsequences of word (as defined above) separated by commas,
+     * with partialSubsequence prepended to each one.
+     */
+    @SuppressWarnings("unused")
+    private static String subsequencesAfter(String partialSubsequence, String word)
+    {
+        return word.isEmpty() ? partialSubsequence
+                : subsequencesAfter(partialSubsequence, word.substring(1)) + ","
+                        + subsequencesAfter(partialSubsequence + word.charAt(0), word.substring(1));
 
-	/**
-	 * @param word
-	 *            consisting only of letters A-Z or a-z
-	 * @return all subsequences of word, separated by commas, where a
-	 *         subsequence is a string of letters found in word in the same
-	 *         order that they appear in word.
-	 */
-	private static String subsequences(String word) {
-		if (word.isEmpty()) {
-			return ""; // base case
-		} else {
-			char firstLetter = word.charAt(0);
-			String restOfWord = word.substring(1);
+        // if (word.isEmpty()) {
+        // // base case
+        // return partialSubsequence;
+        // } else {
+        // // recursive step
+        // return subsequencesAfter(partialSubsequence, word.substring(1)) +
+        // ","
+        // + subsequencesAfter(partialSubsequence + word.charAt(0),
+        // word.substring(1));
+        // }
+    }
 
-			String subsequencesOfRest = subsequences(restOfWord);
+    /**
+     * @param word consisting only of letters A-Z or a-z
+     * @return all subsequences of word, separated by commas, where a
+     *         subsequence is a string of letters found in word in the same
+     *         order that they appear in word.
+     */
+    private static String subsequences(String word)
+    {
+        if (word.isEmpty())
+        {
+            return ""; // base case
+        }
+        else
+        {
+            char firstLetter = word.charAt(0);
+            String restOfWord = word.substring(1);
 
-			String result = "";
-			for (String subsequence : subsequencesOfRest.split(",", -1)) {
-				result += "," + subsequence;
-				result += "," + firstLetter + subsequence;
-			}
-			result = result.substring(1); // remove extra leading comma
-			return result;
-		}
-	}
+            String subsequencesOfRest = subsequences(restOfWord);
+
+            String result = "";
+            for (String subsequence : subsequencesOfRest.split(",", -1))
+            {
+                result += "," + subsequence;
+                result += "," + firstLetter + subsequence;
+            }
+            result = result.substring(1); // remove extra leading comma
+            return result;
+        }
+    }
 
 }
-
